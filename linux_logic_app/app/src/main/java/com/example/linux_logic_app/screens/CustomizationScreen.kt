a32904f57685b96ee774d5fa3c437257c0aa954a
Batscho <bacho08lasha08@gmail.com>
2025-03-14 18:59:39 +0100

    DA Linux Logic - Frontend App [Commit Implementation 2]: Die Projektstruktur wurde verändert und irrelevante und unbenutzte Files gelöscht.

--- linux_logic_app/app/src/main/java/com/example/linux_logic_app/screens/CustomizationScreen.kt
+++ linux_logic_app/app/src/main/java/com/example/linux_logic_app/screens/CustomizationScreen.kt
@@ -54,7 +54,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.linux_logic_app.components.ColorPicker
-import com.example.linux_logic_app.components.PreviewTerminal
+import com.example.linux_logic_app.components.terminal.PreviewTerminal
 import com.example.linux_logic_app.components.UserViewModel
 import com.example.linux_logic_app.ui.theme.LiloBlue
 import com.example.linux_logic_app.ui.theme.LiloDanger
